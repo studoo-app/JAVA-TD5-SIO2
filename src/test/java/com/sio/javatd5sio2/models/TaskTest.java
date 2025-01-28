8dfb232bb168b377873550abe52c73353cd1f8fc
JPechberty <j.pechberty@gmail.com>
2025-01-28 22:58:14 +0100

    Test report error

--- src/test/java/com/sio/javatd5sio2/models/TaskTest.java
+++ src/test/java/com/sio/javatd5sio2/models/TaskTest.java
@@ -18,8 +18,8 @@ class TaskTest {
     void testTaskInitialization() {
         Task newTask = new Task("New task");
         assertEquals("New task", newTask.getTitle());
-        assertFalse(newTask.isCompleted());
-        //assertTrue(newTask.isCompleted());
+        //assertFalse(newTask.isCompleted());
+        assertTrue(newTask.isCompleted());
     }
 
     @Test
